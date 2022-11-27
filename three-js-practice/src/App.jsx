6a2482282bd32b0902e4da003962c178ac5ddfc6
Oscar Harron <oscarharron@hotmail.com>
2022-11-27 18:13:03 +1300

    update initial starting positions

--- three-js-practice/src/App.jsx
+++ three-js-practice/src/App.jsx
@@ -40,10 +40,10 @@ function App() {
       return {amount, direction, color, position}
     }
     const s1 = setShape(0.01, 'x', 'blue', 0)
-    const s2 = setShape(0.01, 'x', 'yellow', 15)
-    const s3 = setShape(0.01, 'y', 'green', -15)
-    const s4 = setShape(0.01, 'y', 'pink', 15)
-    const s5 = setShape(0.01, 'z', 'aqua', -15
+    const s2 = setShape(0.01, 'x', 'yellow', 13)
+    const s3 = setShape(0.01, 'y', 'green', -13)
+    const s4 = setShape(0.01, 'y', 'pink', 13)
+    const s5 = setShape(0.01, 'z', 'aqua', -13)
     )
 
     const shapes = [s1,s2,s3,s4,s5]
