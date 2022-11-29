8b73434d67241550134a25e27ac6072724376d79
Oscar Harron <oscarharron@hotmail.com>
2022-11-29 16:10:02 +1300

    refctor three js to own component

--- three-js-practice/src/App.jsx
+++ three-js-practice/src/App.jsx
@@ -1,109 +1,19 @@
 import { useEffect, useRef } from 'react'
 import reactLogo from './assets/react.svg'
-import * as THREE from 'three'
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
-import spaceImg from '../public/space.jpeg'
-import oscarImg from '../public/pic.jpeg'
-import moonImg from '../public/moon.jpg'
-import textureImg from '../public/purp.jpeg'
+
 import './App.css'
 
 function App() {
-  const mountRef = useRef(null)
-
-  useEffect(() => {
-    const scene = new THREE.Scene()
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000)
- 
-    const renderer = new THREE.WebGLRenderer()
-    renderer.setPixelRatio(window.devicePixelRatio)
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    camera.position.setZ(30)
-    mountRef.current.appendChild(renderer.domElement)
-    renderer.render(scene, camera)
-
-    const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
-    const material = new THREE.MeshStandardMaterial({color: 0xFF6347})
-    const torus = new THREE.Mesh(geometry, material)
-    scene.add(torus)
-
-    const pointLight = new THREE.PointLight(0x00FF00)
-    pointLight.position.set(5,5,20)
-
-    const ambientLight = new THREE.AmbientLight(0xffffff)
-
-    const lightHelper = new THREE.PointLightHelper(pointLight)
-    const gridHelper = new THREE.GridHelper(200, 50)
-
-    scene.add(ambientLight)
-    // scene.add(lightHelper)
-    scene.add(gridHelper)
-
-    const controls = new OrbitControls(camera, renderer.domElement)
 
 
-    function addStar() {
-      const geometry = new THREE.SphereGeometry(0.05, 24, 24)
-      const material = new THREE.MeshStandardMaterial({color: 0xFFE8E8})
-      const star = new THREE.Mesh(geometry, material)
-      const [x, y, z]= Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
-      star.position.set(x, y, z)
-      scene.add(star)
-    }
-    Array(200).fill().forEach(addStar)
+  return <main>
+    <div className='test'><h1>hello</h1></div>
+    <div className='test'>hi</div>
+    <div className='test'>hi</div>
+    <div className='test'>hi</div>
+    <div className='test'>hi</div>
 
-    const spaceTexture = new THREE.TextureLoader().load(spaceImg)
-    scene.background = spaceTexture
-    
-    const oscarTexture = new THREE.TextureLoader().load(oscarImg)
-
-    const oscar = new THREE.Mesh(
-      new THREE.BoxGeometry(3,3,3),
-      new THREE.MeshBasicMaterial({map:oscarTexture})
-    )
-
-    scene.add(oscar)
-
-    const moonTexture = new THREE.TextureLoader().load(moonImg)
-    const bumps = new THREE.TextureLoader().load(textureImg)
-
-    const moon = new THREE.Mesh(
-      new THREE.SphereGeometry(3, 32, 32),
-      new THREE.MeshStandardMaterial({
-        map: moonTexture,
-        normalMap: bumps
-      })
-    )
-    moon.position.setX(10)
-    moon.position.setY(17)
-    scene.add(moon)
-    
-
-
-
-    // animate
-    function animate() {
-      requestAnimationFrame(animate)
-      torus.rotation.x += 0.01;
-      torus.rotation.y += 0.005;
-      torus.rotation.z += 0.01;
-      controls.update()
-      renderer.render(scene, camera)
-    }
-
-    const onWindowResize = () => {
-      camera.aspect = window.innerWidth / window.innerHeight
-      camera.updateProjectionMatrix()
-      renderer.setSize(window.innerWidth, window.innerHeight)
-    }
-
-    window.addEventListener('resize', onWindowResize, false)
-    animate()
-
-    return () => mountRef.current.removeChild(renderer.domElement)
-  }, [])
-
-  return <div ref={mountRef}></div>
+  </main>
 }
 
 export default App
--- three-js-practice/src/components/Scene.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import * as THREE from 'three'
-import Line from './Line'
-export default function Scene() {
-  const scene = new THREE.Scene()
-  scene.add(Line)
-
-  return scene
-}
